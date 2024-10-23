f61541ce06dffe1b3d50969bfb29635b24178d35
jyoti-joshi758 <jj2412josh@gmail.com>
2024-10-23 22:58:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./*.html"  // This tells Tailwind to scan all HTML files in the root folder
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
